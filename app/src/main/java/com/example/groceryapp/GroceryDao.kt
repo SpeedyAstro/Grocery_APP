c8ee16d815d1ee604fc52cd1ee1597544ccd9ec6
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-09-18 18:35:57 +0530

    Grocery-App

---
+++ app/src/main/java/com/example/groceryapp/GroceryDao.kt
@@ -0,0 +1,16 @@
+package com.example.groceryapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface GroceryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item:GroceryItems)
+
+    @Delete
+    suspend fun delete(item:GroceryItems)
+
+    @Query("SELECT * FROM grocerry_items")
+    fun getAllGroceryItems() : LiveData<List<GroceryItems>>
+}
---
+++ app/src/main/java/com/example/groceryapp/GroceryViewModalFactory.kt
@@ -0,0 +1,10 @@
+package com.example.groceryapp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class GroceryViewModalFactory (private val repository: GroceryRepository) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T: ViewModel> create(modelClass: Class<T>):T{
+        return GroceryViewModal(repository) as T
+    }
+}
